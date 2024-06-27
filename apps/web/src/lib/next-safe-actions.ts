835da45dfe908e7ac165a11c89e41064637c61df
chihabMe <chihab.mg.me@gmail.com>
2024-06-27 15:09:13 +0100

    added redirect in the protected action client

--- apps/web/src/lib/next-safe-actions.ts
+++ apps/web/src/lib/next-safe-actions.ts
@@ -1,6 +1,7 @@
 import { createSafeActionClient } from "next-safe-action";
 import { auth } from "./auth";
 import { db } from "./db";
+import { redirect } from "next/navigation";
 
 export const actionClient = createSafeActionClient().use(
   async ({ next, ctx }) => {
@@ -9,6 +10,7 @@ export const actionClient = createSafeActionClient().use(
 );
 export const protectedActionClient = actionClient.use(async ({ next, ctx }) => {
   const session = await auth();
-  if (!session) throw new Error("Unauthorized");
+  // if (!session) throw new Error("Unauthorized");
+  if (!session) redirect("/auth/login");
   return next({ ctx: { ...ctx, session } });
 });
